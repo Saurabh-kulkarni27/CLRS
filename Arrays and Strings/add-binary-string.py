9ab1aa46a285cdb108b939d3264a97cb57f9b8b9
Saurabh Kulkarni <saurabh.kulkarni27421@gmail.com>
2018-10-10 23:45:35 +0100

    Update add-binary-string.py

--- Arrays and Strings/add-binary-string.py
+++ Arrays and Strings/add-binary-string.py
@@ -1,3 +1,14 @@
+'''
+Question: leetcode
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example:
+Input: a = "11", b = "1"
+Output: "100"
+'''
+
+
 class Solution:
     def addBinary(self, a, b):
         """
